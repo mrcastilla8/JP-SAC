83c6ed5a2efe8dcddcc82f29fa0fcc66fa7f6528
Marco Castilla <marco.castilla@unmsm.edu.pe>
2026-05-27 14:22:37 -0500

    Agregado motor de Búsqueda Global Integrada

--- Desarrollo/SGPI/Despliegue/backend/app/SGPI-CAPIRESTC/sgpi_capirestc/api/v1/api.py
+++ Desarrollo/SGPI/Despliegue/backend/app/SGPI-CAPIRESTC/sgpi_capirestc/api/v1/api.py
@@ -7,7 +7,7 @@ from sgpi_capirestc.api.v1.endpoints import calls
 from sgpi_capirestc.api.v1.endpoints import users
 from sgpi_capirestc.api.v1.endpoints import publications
 from sgpi_capirestc.api.v1.endpoints import theses
-from sgpi_capirestc.api.v1.endpoints import search
+from router import router as cbapi_router
 
 api_router = APIRouter()
 
@@ -18,4 +18,4 @@ api_router.include_router(calls.router, prefix="/calls", tags=["calls"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(publications.router, prefix="/publications", tags=["publications"])
 api_router.include_router(theses.router, prefix="/theses", tags=["theses"])
-api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(cbapi_router, prefix="/search", tags=["search"])
